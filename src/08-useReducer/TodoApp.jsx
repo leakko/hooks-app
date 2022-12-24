2fa35a4d8b6936920a91f19c0121f7ce86232850
Marcos Rodriguez Salvo <marcossalvo95@gmail.com>
2022-12-24 16:33:41 +0100

    Add and remove todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,27 +1,43 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import { TodoAdd } from "./TodoAdd";
 import { todoReducer } from "./todoReducer";
 import { TodoList } from "./TodoList";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Recolectar la piedra del alma',
-        done: false,
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del tiempo',
-        done: false,
-    }
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // }
 ];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
 
 export const TodoApp = () => {
 
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState );
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
 
     const handleNewTodo = (todo) => {
-        console.log(todo);
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        // console.log({id})
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
     }
 
   return (
@@ -31,7 +47,7 @@ export const TodoApp = () => {
 
         <div className="row">
             <div className="col-7">
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onDeleteTodo={handleDeleteTodo}/>
             </div>
 
             <div className="col-5">
